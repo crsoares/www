188105a1c524f6eb831d88cdf0dd5e83ad393df0
crysthianophp <crysthianophp@gmail.com>
2014-05-29 19:27:03 -0300

    estudos casa

---
+++ module/Sample/Module.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sample;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module
+{
+	public function getConfig()
+	{
+		return include __DIR__ . '/../config/module.config.php';
+	}
+
+	public function getAutoloaderConfig()
+	{
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				)
+			)	
+		);
+	}
+}
